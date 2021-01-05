01b9a1e513be750685dfafc81ec2fef00a4cff83
Martyna Karbownik <karbownikmartyna984@gmail.com>
2021-01-05 10:30:21 +0100

    Klasy

---
+++ System obsługi konferencji/ListaPrelegentówKonferencji.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_obsługi_konferencji
+{
+    class ListaPrelegentówKonferencji
+    {
+        private string idKonferencji;
+        public LinkedList<Prelegent> listaPrelegentow;
+        private LinkedList<Prelegent> Lista { get => listaPrelegentow; set => listaPrelegentow = value; }
+        public ListaPrelegentów()
+        {
+            listaPrelegentow = new LinkedList<Prelegent>();
+        }
+        public void DodajPrelegenta(Prelegent prelegent)
+        {
+            listaPrelegentow.AddLast(prelegent);
+        }
+    }
+}

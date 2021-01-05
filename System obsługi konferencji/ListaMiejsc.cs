235869ccb1e5452dba306e7b83d9499bbe2a91bf
Martyna Karbownik <karbownikmartyna984@gmail.com>
2021-01-05 10:28:21 +0100

    Klasy

---
+++ System obsługi konferencji/ListaMiejsc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_obsługi_konferencji
+{
+    class ListaMiejsc
+    {
+        public LinkedList<MiejsceKonferencji> listaMiejsc;
+        private LinkedList<MiejsceKonferencji> Lista { get => listaMiejsc; set => listaMiejsc = value; }
+
+        public ListaMiejsc()
+        {
+            listaMiejsc = new LinkedList<MiejsceKonferencji>();
+        }
+
+        public void DodajMiejsce(MiejsceKonferencji miejsce)
+        {
+            listaMiejsc.AddLast(miejsce);
+        }
+    }
+}

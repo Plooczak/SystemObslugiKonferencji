0b0b8c11362233806d68a04bcdf8bed8361e817c
Martyna Karbownik <karbownikmartyna984@gmail.com>
2021-01-05 10:24:51 +0100

    Klasy

--- System obsługi konferencji/Konferencja.cs
+++ System obsługi konferencji/Konferencja.cs
@@ -5,13 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
 
-namespace ProgramoProjekt
+namespace System_obsługi_konferencji
 {
     class Konferencja
     {
+        private string idKonferencji;
+        private string temat;
         private DateTime dataKonferencji;
+        private DateTime terminRejestracji; //data konferencji - 7 dni
+        private Organizator organizator;
+        private ListaPrelegentówKonferencji prelegenci;
+        private ListaUczestnikówKonferencji uczestnicy;
+        private PlanKonferencji plan;
+        private MiejsceKonferencji miejsce;
 
+        public string IdKonferencji { get => idKonferencji; set => idKonferencji = value; }
+        public string Temat { get => temat; set => temat = value; }
         public DateTime DataKonferencji { get => dataKonferencji; set => dataKonferencji = value; }
+        public DateTime TerminRejestracji { get => terminRejestracji; set => terminRejestracji = value; }
+        internal Organizator Organizator { get => organizator; set => organizator = value; }
+        internal ListaPrelegentówKonferencji Prelegenci { get => prelegenci; set => prelegenci = value; }
+        internal ListaUczestnikówKonferencji Uczestnicy { get => uczestnicy; set => uczestnicy = value; }
+        internal PlanKonferencji Plan { get => plan; set => plan = value; }
+        internal MiejsceKonferencji Miejsce { get => miejsce; set => miejsce = value; }
 
         public Konferencja()
         {

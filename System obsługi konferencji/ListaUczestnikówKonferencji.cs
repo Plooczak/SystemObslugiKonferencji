9d044be3dc87bda6091270361a9bdb3b53b3a70f
Martyna Karbownik <karbownikmartyna984@gmail.com>
2021-01-05 10:31:18 +0100

    Klasy

---
+++ System obsługi konferencji/ListaUczestnikówKonferencji.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_obsługi_konferencji
+{
+    class ListaUczestnikówKonferencji
+    {
+        private string idKonferencji;
+        public LinkedList<Uzytkownik> listaUczestnikow;
+        private LinkedList<Uzytkownik> Lista { get => listaUczestnikow; set => listaUczestnikow = value; }
+
+        public ListaUczestnikówKonferencji()
+        {
+            listaUczestnikow = new LinkedList<Uzytkownik>();
+        }
+
+        public void DodajUczestnika(Uzytkownik uzytkownik)
+        {
+            listaUczestnikow.AddLast(uzytkownik);
+        }
+    }
+}

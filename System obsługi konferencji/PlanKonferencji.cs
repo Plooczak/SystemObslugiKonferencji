04fc66c61561a483b42fb3c6415883b4934c9718
Martyna Karbownik <karbownikmartyna984@gmail.com>
2021-01-05 10:33:38 +0100

    Klasy

---
+++ System obsługi konferencji/PlanKonferencji.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_obsługi_konferencji
+{
+    class PlanKonferencji
+    {
+        private string idKonferencji;
+        public LinkedList<Referat> planKonferencji;
+        private LinkedList<Referat> Lista { get => planKonferencji; set => planKonferencji = value; }
+
+        public PlanKonferencji()
+        {
+            planKonferencji = new LinkedList<Referat>();
+        }
+
+        public void DodajUczestnika(Referat referat)
+        {
+            planKonferencji.AddLast(referat);
+        }
+    }
+}
+

8762574d1daef4f6ffef08ed752d5c8835706730
Martyna Karbownik <karbownikmartyna984@gmail.com>
2021-01-05 10:26:23 +0100

    Klasy

---
+++ System obsługi konferencji/ListaKonferencji.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace System_obsługi_konferencji
+{
+    class ListaKonferencji
+    {
+        public LinkedList<Konferencja> listaKonferencji;
+        private LinkedList<Konferencja> Lista { get => listaKonferencji; set => listaKonferencji = value; }
+
+        public ListaKonferencji()
+        {
+            listaKonferencji = new LinkedList<Konferencja>();
+        }
+
+        public void DodajKonferencje(Konferencja konferencja)
+        {
+            listaKonferencji.AddLast(konferencja);
+        }
+
+        public void SortujPoDacie()
+        {
+            List<Konferencja> nowa_lista = new List<Konferencja>(listaKonferencji);
+            nowa_lista.Sort(new DataComparator());
+            listaKonferencji = new LinkedList<Konferencja>(nowa_lista);
+        }
+
+    }
+
+    public class DataComparator : IComparer<Konferencja>
+    {
+        int IComparer<Konferencja>.Compare(Konferencja x, Konferencja y)
+        {
+            if (x != null && y != null)
+            {
+                return x.DataKonferencji.CompareTo(y.DataKonferencji);
+            }
+
+            return 0;
+        }
+    }
+}

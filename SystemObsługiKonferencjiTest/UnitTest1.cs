72a757b78ae4ca76b851ed60ad19d1e47fa92d8d
kjakobik@student.agh.edu.pl <kjakobik@student.agh.edu.pl>
2021-01-06 19:12:50 +0100

    xxx

---
+++ SystemObsługiKonferencjiTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using System_obsługi_konferencji;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SystemObsługiKonferencjiTesty
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestCompareToDane() //test sprawdzajacy metode CompareToDane (porownuje organizatorow po imieniu i nazwisku)
+        {
+            //Organizator o1 = new Organizator("Jan", "Nowak");
+            //Organizator o2 = new Organizator("Adam", "Kowalski");
+            //Assert.AreEqual(1, o1.CompareToDane(o2)); // jesli sa rozne, test bedzie pozytywny, jesli dane sa identyczne test zakonczy sie niepowodzeniem
+        }
+    }
+}

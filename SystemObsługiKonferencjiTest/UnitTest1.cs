5e26bdb3a515a7be4dc8a7053efeec88a017405a
karox9 <karolina.jakobik1999@gmail.com>
2021-01-07 23:18:37 +0100

    testyyy

--- SystemObsługiKonferencjiTest/UnitTest1.cs
+++ SystemObsługiKonferencjiTest/UnitTest1.cs
@@ -27,5 +27,67 @@ namespace SystemObsługiKonferencjiTesty
             Assert.AreEqual(2, list.LiczbaUczestnikow); 
             //pozytywny test oznacza, ze licznik uczestnikow dziala poprawnie
         }
+
+        [TestMethod]
+        public void TestListyPrelegentow() //sprawdzanie metody dodawania prelegentow konferencji 
+        {
+            ListaPrelegentówKonferencji list = new ListaPrelegentówKonferencji();
+            Prelegent p1 = new Prelegent("login1", "haslo1");
+            Prelegent p2 = new Prelegent("login2", "haslo2");
+            list.DodajPrelegenta(p1);
+            list.DodajPrelegenta(p2);
+            Assert.IsNotNull(list);
+            //pozytywny test oznacza, ze licznik prelegentow dziala poprawnie - lista nie jest pusta
+        }
+
+        [TestMethod]
+        public void TestCzyPrzekroczono() //test metody sprawdzajacej czy przekroczono liczbe uczetnikow konferencji
+        {
+            ListaUczestnikówKonferencji list = new ListaUczestnikówKonferencji();
+            Sluchacz s1 = new Sluchacz("login1", "haslo1");
+            Sluchacz s2 = new Sluchacz("login2", "haslo2");
+            Sluchacz s3 = new Sluchacz("login3", "haslo3");
+            Sluchacz s4 = new Sluchacz("login4", "haslo4");
+            Sluchacz s5 = new Sluchacz("login5", "haslo5");
+            Sluchacz s6 = new Sluchacz("login6", "haslo6");
+            list.DodajUczestnika(s1);
+            list.DodajUczestnika(s2);
+            list.DodajUczestnika(s3);
+            list.DodajUczestnika(s4);
+            list.DodajUczestnika(s5);
+            list.DodajUczestnika(s6);
+            MiejsceKonferencji m = new MiejsceKonferencji("Kwiatowa", "Krakow", "30-332", "5", "6", 1, 5);
+            Assert.IsTrue(m.CzyPrzekroczono(list));
+            //pozytywny test oznacza, ze nie przekroczono
+        }
+
+        [TestMethod]
+        public void TestDodajCzasTrwania() //test metody dodajacej czas trwania wystapienia na konferencji
+        {
+            TimeSpan czas1 = new TimeSpan(1, 30, 0);
+            Prelegent p1 = new Prelegent("login", "haslo");
+            Referat r1 = new Referat("tytul", "streszczenie", p1);
+            Assert.AreEqual(czas1, r1.DodajCzasTrwania(1, 30));
+            //pozytywny wynik oznacza, ze czasy sa identyczne
+        }
+
+        [TestMethod]
+        public void TestKopiuj() //test metody kopiuj, ktora klonuje liste uczestnikow
+        {
+            //ListaUczestnikówKonferencji list = new ListaUczestnikówKonferencji();
+            //Sluchacz s1 = new Sluchacz("login1", "haslo1");
+            //Sluchacz s2 = new Sluchacz("login2", "haslo2");
+            //Sluchacz s3 = new Sluchacz("login3", "haslo3");
+            //Sluchacz s4 = new Sluchacz("login4", "haslo4");
+            //Sluchacz s5 = new Sluchacz("login5", "haslo5");
+            //list.DodajUczestnika(s1);
+            //list.DodajUczestnika(s2);
+            //list.DodajUczestnika(s3);
+            //list.DodajUczestnika(s4);
+            //list.DodajUczestnika(s5);
+            
+            //ListaUczestnikówKonferencji list2 = list.Kopiuj();
+            //Assert.AreEqual(list, list2);
+        }
     }
 }
